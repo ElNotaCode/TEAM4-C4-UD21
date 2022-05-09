e02181b870132449b63baa92a5841b1aa261638d
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-09 13:03:20 +0200

    comit de las funcinalidades del controlador

--- CalculadoraEstandar/src/main/java/JUnit/TCS21/Calculadora/CalculadoraEstandar/controller/Controller.java
+++ CalculadoraEstandar/src/main/java/JUnit/TCS21/Calculadora/CalculadoraEstandar/controller/Controller.java
@@ -1,18 +1,14 @@
 package JUnit.TCS21.Calculadora.CalculadoraEstandar.controller;
 
 public class Controller {
-
-	//getLblCantidad
-	public String getLblCantidad(String value) {
-		return value;
-	}
 	
+	//recoger los datos de el add event listener y actualizar el label de CANTIDAD
 	//montarLblCantidad lo que hace es coger el getValue de lbl y de el boton pulsado y los concatena
 		public String montarLblCantidad(String lblValue, String getTextBoton) {
 			return lblValue+getTextBoton;
 		}
-	//String lblCantidad = "";
 	
+	//recoger los datos de el add event listener btn CALCULAR y pasar el label de CANTIDAD a un DOUBLE para el metodo
 	//metodo insertarCantidadConvertir(String value) que pasa el valor del lblCantidad a double
 	public double cantidadConvertir(String lblValue) {
 		//toDouble
@@ -22,51 +18,69 @@ public class Controller {
 	
 	//select divisas
 	//cambioDivisas(String value,String value2) lo que va a hacer es coger los values del select divisa1 y divisa2 y procesará la info
-	public void cambioDivisa1(String divisa1, String divisa2) {
+	public String cambioDivisa(String lblValue,String divisa1, String divisa2) {
 		
-		//no se si sería mejor transformar las string y pasarlas al metodo de Divisas
+		//comparar divisa1 y divisa2 y llamar al metodo correspondiente
 		
 		switch (divisa1) {
-		case "Europa - Euro":
-			cambioDivisa2(divisa2);
+		case "Euro":
+			switch (divisa2) {
+			case "Euro":
+				//mismo valor
+				break;
+			case "Libra":
+				return convertidoToString(Divisa.calcularEuroLibra(cantidadConvertir(lblValue)));
+				break;
+			case "Dolar":
+				return convertidoToString(Divisa.calcularEuroDolar(cantidadConvertir(lblValue)));
+				break;
+			default:
+				break;
+			}
 			break;
-		case "Estados Unidos - Dólar":
-			cambioDivisa2(divisa2);
+		case "Libra":
+			switch (divisa2) {
+			case "Euro":
+				return convertidoToString(Divisa.calcularLibraEuro(cantidadConvertir(lblValue)));
+				break;
+			case "Libra":
+				//mismo valor
+				break;
+			case "Dolar":
+				return convertidoToString(Divisa.calcularLibraDolar(cantidadConvertir(lblValue)));
+				break;
+			default:
+				break;
+			}
 			break;
-		case "Reino Unido - Libras":
-			cambioDivisa2(divisa2);
+		case "Dolar":
+			switch (divisa2) {
+			case "Euro":
+				return convertidoToString(Divisa.calcularDolarEuro(cantidadConvertir(lblValue)));
+				break;
+			case "Libra":
+				return convertidoToString(Divisa.calcularDolarLibra(cantidadConvertir(lblValue)));
+				break;
+			case "Dolar":
+				//mismo valor
+				break;
+			default:
+				break;
+			}
 			break;
-
 		default:
-			//TODO: pasar un mensaje de error
-			break;
-		}
-		
-	}
-	
-	//TODO: terminar
-	public void cambioDivisa2(String divisa2) {
-		switch (divisa2) {
-		case "Europa - Euro":
-			//metodo calcular 
-			break;
-		case "Estados Unidos - Dólar":
-			
-			break;
-		case "Reino Unido - Libras":
-			
-			break;
-
-		default:
-			//TODO:pasar un mensaje de error
 			break;
 		}
 	}
 	
 	//lblConvertido cantidad una vez procesada
 	//pasa de double a string
-	public String getConvertido(double value) {
-		
+	public String convertidoToString(double value) {
 		return Double.toString(value);
+	}
+	
+	//metodo para borrar
+	public String borrar() {
+		return "";
 	}
 }
